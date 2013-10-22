8e6c668cada0c22b40b1324a9d9755434b9185a5
alainlap <alainlaplume@gmail.com>
2013-10-22 13:36:20 -0400

    created Person, Instructor and Student classes

---
+++ people.rb
@@ -0,0 +1,37 @@
+class Person
+	attr_accessor :name
+
+	def initialize name
+		@name = name
+	end
+
+	def greeting
+		puts "Hi my name is #{@name}"
+	end
+end
+
+class Student < Person
+
+	def learn
+
+		puts "I get it!"
+	end
+end
+
+class Instructor < Person
+
+	def teach
+
+		puts "Everything in Ruby is an Object"
+	end
+end
+
+
+Chris = Instructor.new("Chris")
+Cristina = Student.new("Christina")
+
+Chris.greeting
+Cristina.greeting
+
+Chris.teach
+Cristina.learn

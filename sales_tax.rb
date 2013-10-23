bc44a525da955e1134481af42ea7564a29c3aa80
alainlap <alainlaplume@gmail.com>
2013-10-23 10:41:39 -0400

    still working on identifying item type

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,32 +1,3 @@
-# INPUT:
-
-# Input 1: 1 book at 12.49 1 music CD at 14.99 1 chocolate bar at 0.85
-# Input 2: 1 imported box of chocolates at 10.00 1 imported bottle of perfume at 47.50
-# Input 3: 1 imported bottle of perfume at 27.99 1 bottle of perfume at 18.99 1 packet of headache pills at 9.75 1 box of imported chocolates at 11.25
-
-# OUTPUT
-
-# Output 1: 1 book : 12.49 1 music CD: 16.49 1 chocolate bar: 0.85 Sales Taxes: 1.50 Total: 29.83
-# Output 2: 1 imported box of chocolates: 10.50 1 imported bottle of perfume: 54.65 Sales Taxes: 7.65 Total: 65.15
-# Output 3: 1 imported bottle of perfume: 32.19 1 bottle of perfume: 20.89 1 packet of headache pills: 9.75 1 imported box of chocolates: 11.85 Sales Taxes: 6.70 Total: 74.68
-
-
-
-# class Goods
-#   # def initialize type, cost
-#   #   @type = type
-#   #   @cost = cost
-#   # end
-# end
-
-# class Taxable_Goods < Goods
-
-# end
-
-# class Exempt_Goods < Goods
-
-# end
-
 class Basket
 
   def initialize str
@@ -40,7 +11,7 @@ class Basket
 
     i = 0
     while i+1 < @str_a.length do
-
+  
       if (@str_a[i].to_f == 0) && (@str_a[i+1].to_f == 0)
         @str_a[i] = @str_a[i] + " " + @str_a[i+1]
         @str_a.delete_at(i+1)
@@ -65,15 +36,29 @@ class Basket
       @new_str_a[n][0] = @str_a[n*3]
       @new_str_a[n][1] = @str_a[n*3+1]
       @new_str_a[n][2] = @str_a[n*3+2]
-    end
+  end
+
+  # def check_type item
+  #   words = item.split(' ')
+    
+  #   words.each do |word|
+  #     word == "imported" ? 'true' : 'false'
+  #   end
+
+
+  # end
+
+  # def calculate_tax item
+  #   if import == true
+  #     price*1.50
+  #   else
+  #     price*1.13
+  #   end
+  # end
+
     @new_str_a
   end
 end
-
-# def calculate_tax price
-#   after_tax = price*1.13
-# end
-
 
 basket_1 = Basket.new("1 book at 12.49 1 music CD at 14.99 1 chocolate bar at 0.85")
 basket_2 = Basket.new("1 imported box of chocolates at 10.00 1 imported bottle of perfume at 47.50")
@@ -83,35 +68,22 @@ basket_3 = Basket.new("1 imported bottle of perfume at 27.99 1 bottle of perfume
 # basket_2.print_string
 # basket_3.print_string
 
-def check_if_imported item
-  if item.includes?('import')
-    true
-  else
-    false
-  end
-end
 
-def calculate_tax price, import
-  if import == true
-    price*1.50
-  else
-    price*1.13
-  end
-end
 
 def generate_receipt str_a
+
   total = 0
   puts "\n You have purchased: \n\n"
   str_a.each do |item|
-    puts "#{item[0]} x #{item[1]}: $#{item[2]*1.13};"
-    total += item[2].to_f
+    puts "#{item[0]} x #{item[1]}: $#{item[2]};"
+    total += item[3].to_f
+
   end
   puts "\n Your total is: \t $#{total.round(2)}"
   puts "\n"
 end
 
 generate_receipt(basket_3.my_items)
-
 
 
 

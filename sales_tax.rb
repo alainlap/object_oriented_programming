4c7f594ba9e4e03f6c7b850d85b318293da909b7
alainlap <alainlaplume@gmail.com>
2013-10-23 15:07:49 -0400

    restarting sales tax calc -- string parsing not necessary

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,97 +1,118 @@
 class Basket
 
-  def initialize str
-    @str = str
+  def initialize basket_contents
+    @basket_contents = basket_contents
   end
 
-  def parse_string
+  def parse_contents
 
-    @str_a = @str.split(' ')
-    @str_a.delete('at')
+    @split = @basket_contents.split(' ')
+    @split.delete('at')
 
+    # rebuild words that belong together, i.e. "bar of chocolate"
     i = 0
-    while i+1 < @str_a.length do
-  
-      if (@str_a[i].to_f == 0) && (@str_a[i+1].to_f == 0)
-        @str_a[i] = @str_a[i] + " " + @str_a[i+1]
-        @str_a.delete_at(i+1)
+    while i+1 < @split.length do
+      # identify words followed by more words (not numbers!)
+      if (@split[i].to_f == 0) && (@split[i+1].to_f == 0)
+        @split[i] = @split[i] + " " + @split[i+1]
+        @split.delete_at(i+1)
         i -= 1
       end
       i += 1
     end
-    return @str_a
+    return @split
   end
 
-  def print_string
-    p parse_string
-  end
-
-  def my_items
-    @str_a = parse_string
-
-    @items = @str_a.length/3
-    @new_str_a = Array.new(@items) {Array.new(3)}
-
-    @items.times do |n|
-      @new_str_a[n][0] = @str_a[n*3]
-      @new_str_a[n][1] = @str_a[n*3+1]
-      @new_str_a[n][2] = @str_a[n*3+2]
-  end
-
-  # def check_type item
-  #   words = item.split(' ')
+  def get_item_from_basket
     
-  #   words.each do |word|
-  #     word == "imported" ? 'true' : 'false'
-  #   end
+    parse_contents
 
+    @item ={
+      quantity: nil,
+      description: nil,
+      price: nil,
+      import: nil,
+      type: nil,
+      tax_rate: nil
+    }
 
-  # end
+    @items = []
 
-  # def calculate_tax item
-  #   if import == true
-  #     price*1.50
-  #   else
-  #     price*1.13
-  #   end
-  # end
+    @no_of_items = @split.length/3
 
-    @new_str_a
+    @no_of_items.times do |n|
+      @item[:quantity] = @split[n*3]
+      @item[:description] = @split[n*3+1]
+      @item[:price] = @split[n*3+2]
+
+      p @item
+      @items << @item
+    end
+
+    # WHY DOESN"T THIS STICK???
+    return @items
   end
+
+  def check_if_import
+    
+    get_item_from_basket
+
+    # @items.each do |i|
+    #   puts i[:description]
+    # end
+  end
+
 end
+  
+
+  # # def check_type item
+  # #   words = item.split(' ')
+    
+  # #   words.each do |word|
+  # #     word == "imported" ? 'true' : 'false'
+  # #   end
+
+
+  # # end
+
+  # # def calculate_tax item
+  # #   if import == true
+  # #     price*1.50
+  # #   else
+  # #     price*1.13
+  # #   end
+  # # end
+
+  #   @new_split
+  # end
+# end
 
 basket_1 = Basket.new("1 book at 12.49 1 music CD at 14.99 1 chocolate bar at 0.85")
 basket_2 = Basket.new("1 imported box of chocolates at 10.00 1 imported bottle of perfume at 47.50")
 basket_3 = Basket.new("1 imported bottle of perfume at 27.99 1 bottle of perfume at 18.99 1 packet of headache pills at 9.75 1 box of imported chocolates at 11.25")
 
-# basket_3.print_string
-# basket_2.print_string
-# basket_3.print_string
+# p basket_1.parse_contents
+# p basket_1.get_item_from_basket
+
+basket_1.check_if_import
 
 
+# p basket_3.item
 
-def generate_receipt str_a
 
-  total = 0
-  puts "\n You have purchased: \n\n"
-  str_a.each do |item|
-    puts "#{item[0]} x #{item[1]}: $#{item[2]};"
-    total += item[3].to_f
+# def generate_receipt split
 
-  end
-  puts "\n Your total is: \t $#{total.round(2)}"
-  puts "\n"
-end
+#   total = 0
+#   puts "\n You have purchased: \n\n"
+#   split.each do |item|
+#     puts "#{item[0]} x #{item[1]}: $#{item[2]};"
+#     total += item[3].to_f
 
-generate_receipt(basket_3.my_items)
+#   end
+#   puts "\n Your total is: \t $#{total.round(2)}"
+#   puts "\n"
+# end
 
-item_h ={
-  quantity: nil,
-  item: nil,
-  cost: nil,
-  import: nil,
-  type: nil,
-  tax_rate: nil
-}
+# generate_receipt(basket_3.my_items)
 
 

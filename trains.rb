6cf5de25886a8dad83ef1c2c938f557780cfbeac
alainlap <alainlaplume@gmail.com>
2013-10-24 11:52:14 -0400

    questions 1 to 5 work

--- trains.rb
+++ trains.rb
@@ -1,65 +1,26 @@
-# Exercise 3: Trains (Bonus)
 
-# Problem: The local commuter railroad services a number of towns in Kiwiland. 
-# Because of monetary concerns, all of the tracks are 'one-way.' That is, a route
-# from Kaitaia to Invercargill does not imply the existence of a route from Invercargill
-# to Kaitaia. In fact, even if both of these routes do happen to exist, they are 
-# distinct and are not necessarily the same distance!
-# 
-# The purpose of this problem is to help the railroad provide its customers with 
-# information about the routes. In particular, you will compute the distance along 
-# a certain route, the number of different routes between two towns, and the shortest 
-# route between two towns.
+module Tools
 
-# Input:  A directed graph where a node represents a town and an edge represents 
-# a route between two towns.  The weighting of the edge represents the distance 
-# between the two towns.  A given route will never appear more than once, and 
-# for a given route, the starting and ending town will not be the same town.
- 
-# Output: For test input 1 through 5, if no such route exists, output 'NO SUCH ROUTE'.
-# Otherwise, follow the route as given; do not make any extra stops!  
-# For example, the first problem means to start at city A, then travel directly 
-# to city B (a distance of 5), then directly to city C (a distance of 4).
- 
-# 1. The distance of the route A-B-C.
-# 2. The distance of the route A-D.
-# 3. The distance of the route A-D-C.
-# 4. The distance of the route A-E-B-C-D.
-# 5. The distance of the route A-E-D.
-# 6. The number of trips starting at C and ending at C with a maximum of 3 stops.  
-#    In the sample data below, there are two such trips: C-D-C (2 stops). and C-E-B-C (3 stops).
-# 7. The number of trips starting at A and ending at C with exactly 4 stops.  
-#    In the sample data below, there are three such trips: A to C (via B,C,D);
-#    A to C (via D,C,D); and A to C (via D,E,B).
-# 8. The length of the shortest route (in terms of distance to travel) from A to C.
-# 9. The length of the shortest route (in terms of distance to travel) from B to B.
-# 10. The number of different routes from C to C with a distance of less than 30.  \
-#    In the sample data, the trips are: CDC, CEBC, CEBCDC, CDCEBC, CDEBC, CEBCEBC, CEBCEBCEBC.
- 
-# Test Input:
- 
-# For the test input, the towns are named using the first few letters of the alphabet from A to E.
- # A route between two towns (A to B) with a distance of 5 is represented as AB5.
- 
-# Graph: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7
- 
-# Expected Output:
- 
-# Output #1: 9
-# Output #2: 5
-# Output #3: 13
-# Output #4: 22
-# Output #5: NO SUCH ROUTE
-# Output #6: 2
-# Output #7: 3
-# Output #8: 9
-# Output #9: 9
-# Output #10: 7
-# ************************************************************
+  def convert_node_to_index node
+    
+    case node
+      when "A"
+        0
+      when "B"
+        1
+      when "C"
+        2
+      when "D"
+        3
+      when "E"
+        4
+    end
+  end
+end
 
 class Station
   
-  # attr_accessor :to_station_A, :to_station_B, :to_station_C, :to_station_D, :to_station_E
+  include Tools
 
   def initialize to_station_A, to_station_B, to_station_C, to_station_D, to_station_E
     
@@ -78,13 +39,28 @@ class Station
     p "The distance to station E is: #{@to_station_E}"
   end
 
-  def distance_matrix
-    [@to_station_A, @to_station_B, @to_station_C, @to_station_D, @to_station_E]
+  def distance_array
+    [
+      @to_station_A,
+      @to_station_B,
+      @to_station_C,
+      @to_station_D,
+      @to_station_E
+    ]
   end
+
+  def print_distance_to node
+    distances = distance_array
+    index = convert_node_to_index(node)
+    distances[index]
+  end
+
 end
 
 class Conductor
   
+  include Tools
+
   def initialize station_A, station_B, station_C, station_D, station_E
     @station_A = station_A
     @station_B = station_B
@@ -94,51 +70,37 @@ class Conductor
   end
 
   def build_matrix
-    a = @station_A.distance_matrix
-    b = @station_B.distance_matrix
-    c = @station_C.distance_matrix
-    d = @station_D.distance_matrix
-    e = @station_E.distance_matrix
+    a = [0,   5,   nil, 5,   7]
+    b = [nil, 0,   4,   nil, nil]
+    c = [nil, nil, 0,   8,   2]
+    d = [nil, nil, 8,   0,   6]
+    e = [nil, 3,   nil, nil, 0]
 
-    [a, b, c, d, e]
+    @matrix = [a, b, c, d, e]
   end
 
-  def print_all_distances
-    @matrix = build_matrix
+  def calculate_distances *nodes
+    total = 0
+    nodes.each_index do |index|
+      
+      if index > 0        
+        temp = distance_between(nodes[index-1], nodes[index])
 
-    @matrix.each_with_index { |distances, origin|
-      @matrix[origin].each_with_index { |distance, destination|
-        p "The distance between #{map_location(origin)} and #{map_location(destination)} is: #{distance}"
-      }
-    }
-  end
-
-  def calculate_distances
-    p @matrix = build_matrix
-
-    @matrix.each_with_index { |distances, origin|
-      @matrix[origin].each_with_index { |distance, destination|
-        if distance != nil 
-          temp = d[:E] + distan
-        elsif (d[key] == nil) || (temp < d[key])
-          d[key] = temp
+        if temp.nil?
+          total = "NO SUCH ROUTE"
+        elsif
+          total += temp
         end
+      end
     end
-
-      }
-    }
+    total
   end
 
-  def map_location index
-
-    map = {
-      0 => 'Station A',
-      1 => 'Station B',
-      2 => 'Station C',
-      3 => 'Station D',
-      4 => 'Station E'
-    }
-    map[index]
+  def distance_between node_1, node_2
+    node_1 = convert_node_to_index(node_1)
+    node_2 = convert_node_to_index(node_2)
+    @matrix = build_matrix
+    @matrix[node_1][node_2]
   end
 end
 
@@ -152,32 +114,21 @@ end
 # Create Conductor objects
 conductor = Conductor.new(station_A, station_B, station_C, station_D, station_E)
 
-
-# TESTING
-# station_A.print_all_distances
-# conductor.print_all_distances
-conductor.calculate_distances
-
-# /TESTING
+# Ask Conductor questions
 
 
+puts "\n 1. The distance of the route A-B-C: #{conductor.calculate_distances('A', 'B', 'C')}"
+puts "\n 2. The distance of the route A-D: #{conductor.calculate_distances('A', 'D')}"
+puts "\n 3. The distance of the route A-D-C: #{conductor.calculate_distances('A', 'D', 'C')}"
+puts "\n 4. The distance of the route A-E-B-C-D: #{conductor.calculate_distances('A', 'E', 'B', 'C', 'D')}"
+puts "\n 5. The distance of the route A-E-D: #{conductor.calculate_distances('A', 'E', 'D')}"
 
-
-# conductor.print_all_distances
-# p solve_with_matrix
-# p map_location(1)
-
-# station_A.distance_to_C(station_B)
-# station_B.distance_to_D(station_C)
-# station_C.distance_to_E(station_D)
-
-# # Check distances
-# station_A.print_all_distances
-# station_B.print_all_distances
-# station_C.print_all_distances
-# station_D.print_all_distances
-# station_E.print_all_distances
-
+puts "\n 6. The number of trips starting at C and ending at C with a maximum of 3 stops.  In the sample data below, there are two such trips: C-D-C (2 stops). and C-E-B-C (3 stops)."
+puts "\n 7. The number of trips starting at A and ending at C with exactly 4 stops.  In the sample data below, there are three such trips: A to C (via B,C,D); A to C (via D,C,D); and A to C (via D,E,B)."
+puts "\n 8. The length of the shortest route (in terms of distance to travel) from A to C."
+puts "\n 9. The length of the shortest route (in terms of distance to travel) from B to B."
+puts "\n 10. The number of different routes from C to C with a distance of less than 30.  In the sample data, the trips are: CDC, CEBC, CEBCDC, CDCEBC, CDEBC, CEBCEBC, CEBCEBCEBC."
+ 
 
 
 
